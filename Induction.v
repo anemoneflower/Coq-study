eafb63595e8773234fa3da4b93428a2a7ad17484
seoyun.son <seoyun.son@desktop-04.maas>
2020-01-17 15:59:04 +0900

    start Induction

---
+++ Induction.v
@@ -0,0 +1,429 @@
+(** * Induction: Proof by Induction *)
+
+(** Before getting started, we need to import all of our
+    definitions from the previous chapter: *)
+
+From LF Require Export Basics.
+
+(** * Proof by Induction *)
+
+
+Theorem plus_n_O_firsttry : forall n:nat,
+  n = n + 0.
+
+Proof.
+  intros n.
+  simpl. (* Does nothing! *)
+Abort.
+
+
+Theorem plus_n_O_secondtry : forall n:nat,
+  n = n + 0.
+Proof.
+  intros n. destruct n as [| n'] eqn:E.
+  - (* n = 0 *)
+    reflexivity. (* so far so good... *)
+  - (* n = S n' *)
+    simpl.       (* ...but here we are stuck again *)
+Abort.
+
+Theorem plus_n_O : forall n:nat, n = n + 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - (* n = 0 *)    reflexivity.
+  - (* n = S n' *) simpl. rewrite <- IHn'. reflexivity.  Qed.
+
+
+Theorem minus_diag : forall n,
+  minus n n = 0.
+Proof.
+  (* WORKED IN CLASS *)
+  intros n. induction n as [| n' IHn'].
+  - (* n = 0 *)
+    simpl. reflexivity.
+  - (* n = S n' *)
+    simpl. rewrite -> IHn'. reflexivity.  Qed.
+
+
+(** **** Exercise: 2 stars, standard, recommended (basic_induction)  
+
+    Prove the following using induction. You might need previously
+    proven results. *)
+
+Theorem mult_0_r : forall n:nat,
+  n * 0 = 0.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem plus_n_Sm : forall n m : nat,
+  S (n + m) = n + (S m).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem plus_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 2 stars, standard (double_plus)  
+
+    Consider the following function, which doubles its argument: *)
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+(** Use induction to prove this simple fact about [double]: *)
+
+Lemma double_plus : forall n, double n = n + n .
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 2 stars, standard, optional (evenb_S)  
+
+    One inconvenient aspect of our definition of [evenb n] is the
+    recursive call on [n - 2]. This makes proofs about [evenb n]
+    harder when done by induction on [n], since we may need an
+    induction hypothesis about [n - 2]. The following lemma gives an
+    alternative characterization of [evenb (S n)] that works better
+    with induction: *)
+
+Theorem evenb_S : forall n : nat,
+  evenb (S n) = negb (evenb n).
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 1 star, standard (destruct_induction)  
+
+    Briefly explain the difference between the tactics [destruct]
+    and [induction].
+
+(* FILL IN HERE *)
+*)
+
+(* Do not modify the following line: *)
+Definition manual_grade_for_destruct_induction : option (nat*string) := None.
+(** [] *)
+
+(* ################################################################# *)
+(** * Proofs Within Proofs *)
+
+
+Theorem mult_0_plus' : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  assert (H: 0 + n = n). { reflexivity. }
+  rewrite -> H.
+  reflexivity.  Qed.
+
+
+Theorem plus_rearrange_firsttry : forall n m p q : nat,
+  (n + m) + (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  (* We just need to swap (n + m) for (m + n)... seems
+     like plus_comm should do the trick! *)
+  rewrite -> plus_comm.
+  (* Doesn't work...Coq rewrites the wrong plus! *)
+Abort.
+
+(** To use [plus_comm] at the point where we need it, we can introduce
+    a local lemma stating that [n + m = m + n] (for the particular [m]
+    and [n] that we are talking about here), prove this lemma using
+    [plus_comm], and then use it to do the desired rewrite. *)
+
+Theorem plus_rearrange : forall n m p q : nat,
+  (n + m) + (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  assert (H: n + m = m + n).
+  { rewrite -> plus_comm. reflexivity. }
+  rewrite -> H. reflexivity.  Qed.
+
+(* ################################################################# *)
+(** * Formal vs. Informal Proof *)
+
+
+Theorem plus_assoc' : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof. intros n m p. induction n as [| n' IHn']. reflexivity.
+  simpl. rewrite -> IHn'. reflexivity.  Qed.
+
+(** Coq is perfectly happy with this.  For a human, however, it
+    is difficult to make much sense of it.  We can use comments and
+    bullets to show the structure a little more clearly... *)
+
+Theorem plus_assoc'' : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [| n' IHn'].
+  - (* n = 0 *)
+    reflexivity.
+  - (* n = S n' *)
+    simpl. rewrite -> IHn'. reflexivity.   Qed.
+
+(** - _Theorem_: For any [n], [m] and [p],
+
+      n + (m + p) = (n + m) + p.
+
+    _Proof_: By induction on [n].
+
+    - First, suppose [n = 0].  We must show
+
+        0 + (m + p) = (0 + m) + p.
+
+      This follows directly from the definition of [+].
+
+    - Next, suppose [n = S n'], where
+
+        n' + (m + p) = (n' + m) + p.
+
+      We must show
+
+        (S n') + (m + p) = ((S n') + m) + p.
+
+      By the definition of [+], this follows from
+
+        S (n' + (m + p)) = S ((n' + m) + p),
+
+      which is immediate from the induction hypothesis.  _Qed_. *)
+
+
+(** **** Exercise: 2 stars, advanced, recommended (plus_comm_informal)  
+
+    Translate your solution for [plus_comm] into an informal proof:
+
+    Theorem: Addition is commutative.
+
+    Proof: (* FILL IN HERE *)
+*)
+
+(* Do not modify the following line: *)
+Definition manual_grade_for_plus_comm_informal : option (nat*string) := None.
+(** [] *)
+
+(** **** Exercise: 2 stars, standard, optional (eqb_refl_informal)  
+
+    Write an informal proof of the following theorem, using the
+    informal proof of [plus_assoc] as a model.  Don't just
+    paraphrase the Coq tactics into English!
+
+    Theorem: [true = n =? n] for any [n].
+
+    Proof: (* FILL IN HERE *)
+
+    [] *)
+
+(* ################################################################# *)
+(** * More Exercises *)
+
+(** **** Exercise: 3 stars, standard, recommended (mult_comm)  
+
+    Use [assert] to help prove this theorem.  You shouldn't need to
+    use induction on [plus_swap]. *)
+
+Theorem plus_swap : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+(** Now prove commutativity of multiplication.  (You will probably
+    need to define and prove a separate subsidiary theorem to be used
+    in the proof of this one.  You may find that [plus_swap] comes in
+    handy.) *)
+
+Theorem mult_comm : forall m n : nat,
+  m * n = n * m.
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 3 stars, standard, optional (more_exercises)  
+
+    Take a piece of paper.  For each of the following theorems, first
+    _think_ about whether (a) it can be proved using only
+    simplification and rewriting, (b) it also requires case
+    analysis ([destruct]), or (c) it also requires induction.  Write
+    down your prediction.  Then fill in the proof.  (There is no need
+    to turn in your piece of paper; this is just to encourage you to
+    reflect before you hack!) *)
+
+Check leb.
+
+Theorem leb_refl : forall n:nat,
+  true = (n <=? n).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem zero_nbeq_S : forall n:nat,
+  0 =? (S n) = false.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem andb_false_r : forall b : bool,
+  andb b false = false.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem plus_ble_compat_l : forall n m p : nat,
+  n <=? m = true -> (p + n) <=? (p + m) = true.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem S_nbeq_0 : forall n:nat,
+  (S n) =? 0 = false.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem mult_1_l : forall n:nat, 1 * n = n.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem all3_spec : forall b c : bool,
+    orb
+      (andb b c)
+      (orb (negb b)
+               (negb c))
+  = true.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem mult_plus_distr_r : forall n m p : nat,
+  (n + m) * p = (n * p) + (m * p).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem mult_assoc : forall n m p : nat,
+  n * (m * p) = (n * m) * p.
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 2 stars, standard, optional (eqb_refl)  
+
+    Prove the following theorem.  (Putting the [true] on the left-hand
+    side of the equality may look odd, but this is how the theorem is
+    stated in the Coq standard library, so we follow suit.  Rewriting
+    works equally well in either direction, so we will have no problem
+    using the theorem no matter which way we state it.) *)
+
+Theorem eqb_refl : forall n : nat,
+  true = (n =? n).
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 2 stars, standard, optional (plus_swap')  
+
+    The [replace] tactic allows you to specify a particular subterm to
+   rewrite and what you want it rewritten to: [replace (t) with (u)]
+   replaces (all copies of) expression [t] in the goal by expression
+   [u], and generates [t = u] as an additional subgoal. This is often
+   useful when a plain [rewrite] acts on the wrong part of the goal.
+
+   Use the [replace] tactic to do a proof of [plus_swap'], just like
+   [plus_swap] but without needing [assert (n + m = m + n)]. *)
+
+Theorem plus_swap' : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  (* FILL IN HERE *) Admitted.
+(** [] *)
+
+(** **** Exercise: 3 stars, standard, recommended (binary_commute)  
+
+    Recall the [incr] and [bin_to_nat] functions that you
+    wrote for the [binary] exercise in the [Basics] chapter.  Prove
+    that the following diagram commutes:
+
+                            incr
+              bin ----------------------> bin
+               |                           |
+    bin_to_nat |                           |  bin_to_nat
+               |                           |
+               v                           v
+              nat ----------------------> nat
+                             S
+
+    That is, incrementing a binary number and then converting it to
+    a (unary) natural number yields the same result as first converting
+    it to a natural number and then incrementing.
+    Name your theorem [bin_to_nat_pres_incr] ("pres" for "preserves").
+
+    Before you start working on this exercise, copy the definitions
+    from your solution to the [binary] exercise here so that this file
+    can be graded on its own.  If you want to change your original
+    definitions to make the property easier to prove, feel free to
+    do so! *)
+
+(* FILL IN HERE *)
+
+(* Do not modify the following line: *)
+Definition manual_grade_for_binary_commute : option (nat*string) := None.
+(** [] *)
+
+(** **** Exercise: 5 stars, advanced (binary_inverse)  
+
+    This is a further continuation of the previous exercises about
+    binary numbers.  You may find you need to go back and change your
+    earlier definitions to get things to work here.
+
+    (a) First, write a function to convert natural numbers to binary
+        numbers. *)
+
+Fixpoint nat_to_bin (n:nat) : bin
+  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+
+(** Prove that, if we start with any [nat], convert it to binary, and
+    convert it back, we get the same [nat] we started with.  (Hint: If
+    your definition of [nat_to_bin] involved any extra functions, you
+    may need to prove a subsidiary lemma showing how such functions
+    relate to [nat_to_bin].) *)
+
+Theorem nat_bin_nat : forall n, bin_to_nat (nat_to_bin n) = n.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+(* Do not modify the following line: *)
+Definition manual_grade_for_binary_inverse_a : option (nat*string) := None.
+
+(** (b) One might naturally expect that we should also prove the
+        opposite direction -- that starting with a binary number,
+        converting to a natural, and then back to binary should yield
+        the same number we started with.  However, this is not the
+        case!  Explain (in a comment) what the problem is. *)
+
+(* FILL IN HERE *)
+
+(* Do not modify the following line: *)
+Definition manual_grade_for_binary_inverse_b : option (nat*string) := None.
+
+(** (c) Define a normalization function -- i.e., a function
+        [normalize] going directly from [bin] to [bin] (i.e., _not_ by
+        converting to [nat] and back) such that, for any binary number
+        [b], converting [b] to a natural and then back to binary yields
+        [(normalize b)].  Prove it.  (Warning: This part is a bit
+        tricky -- you may end up defining several auxiliary lemmas.
+        One good way to find out what you need is to start by trying
+        to prove the main statement, see where you get stuck, and see
+        if you can find a lemma -- perhaps requiring its own inductive
+        proof -- that will allow the main proof to make progress.) Don't
+        define thi using nat_to_bin and bin_to_nat! *)
+
+(* FILL IN HERE *)
+
+(* Do not modify the following line: *)
+Definition manual_grade_for_binary_inverse_c : option (nat*string) := None.
+(** [] *)
+
